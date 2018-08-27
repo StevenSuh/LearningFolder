db8a723a927e3fb00e7e26d96432bd6d96848b62
Steven Suh <stevenesuh@gmail.com>
2018-08-27 01:23:42 -0700

    Comment box setup for test

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -5,14 +5,18 @@ import App from "components/App";
 import CommentBox from "components/CommentBox";
 import CommentList from "components/CommentList";
 
-it("renders a comment box", () => {
-    const app_wrapped = shallow(<App />);
+// setup
+let wrapped;
 
+beforeEach(() => {
+    app_wrapped = shallow(<App />);
+});
+// setup end
+
+it("renders a comment box", () => {
     expect(app_wrapped.find(CommentBox).length).toEqual(1);
 });
 
 it("renders a comment list", () => {
-    const app_wrapped = shallow(<App />);
-
     expect(app_wrapped.find(CommentList).length).toEqual(1);
 });
