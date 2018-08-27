0e18ad7962311b3682d6a1c0915e967b4187081a
Steven Suh <stevenesuh@gmail.com>
2018-08-26 18:42:43 -0700

    First commit

---
+++ src/components/__tests__/App.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../App';
+
+it('renders a comment box', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+
+    // start test
+    expect(div.innerHTML).toContain('Comment Box');
+    // end test
+
+    ReactDOM.unmountComponentAtNode(div);
+});
